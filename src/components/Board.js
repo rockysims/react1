d3f1f76e8f30c3c86011f0bbbbbbf77afde3cc27
Rocky Sims <rocky.sims@gmail.com>
2019-12-22 00:09:15 -0800

    Added some basic drag and drop to board.

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,16 +1,56 @@
 import React from 'react';
 import './Board.css';
 
-export default function Board({ board }) {
+export default function Board({ board, onDragCellChanged }) {
+	//call setDragCell() as appropriate
+	let mouseDown = false;
+	function onEnterCell(e) {
+		if (mouseDown) setDragCell(e.target);
+	}
+	function onMouseDown(e) {
+		if (e.target && e.target.classList.contains('cell')) {
+			setDragCell(e.target);
+		}
+		mouseDown = true;
+	}
+	function onMouseUp(e) {
+		setDragCell(null);
+		mouseDown = false;
+	}
+	function onLeaveBoard(e) {
+		setDragCell(null);
+	}
+	function onLeaveBoard() {
+		setDragCell(null);
+	}
+
+	let dragCell = null;
+	function setDragCell(newDragCell) {
+		if (newDragCell && !newDragCell.classList) return; //elements only (or falsy)
+
+		if (dragCell) dragCell.classList.remove('dragTarget');
+		if (newDragCell) newDragCell.classList.add('dragTarget');
+		if (onDragCellChanged && newDragCell !== dragCell)
+			onDragCellChanged(newDragCell, dragCell);
+		dragCell = newDragCell;
+	}
+
 	return (
-		<div className='board'>{
+		<div className='board' onMouseLeave={onLeaveBoard}>{
 			board.map((row, r) =>
 				<div className='row' key={r}>{
-			row.map((cellValue, c) =>
-				<div className={`cell ${cellValue}`} key={`${r},${c}`}>
-					{cellValue[0]}
-				</div>)
-			}</div>)
+					row.map((cellValue, c) =>
+						<div className={`cell ${cellValue}`}
+							r={r}
+							c={c}
+							key={`${r},${c}`}
+							onMouseEnter={onEnterCell}
+							onMouseDown={onMouseDown}
+							onMouseUp={onMouseUp}
+						>
+							{cellValue[0]}
+						</div>)
+				}</div>)
 		}</div>
 	);
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,46 @@ import Board from './Board';
 
 export default class Main extends Component {
 	state = {
-		board: []
+		board: [],
+		drag: {
+			sourceCell: null,
+			targetCell: null
+		}
+	}
+
+	onDragCellChanged(dragCell, oldDragCell) {
+		const drag = this.state.drag;
+
+		if (oldDragCell === null) {
+			//start drag
+			drag.sourceCell = dragCell;
+			drag.sourceCell.classList.add('dragSource');
+		} else if (dragCell === null) {
+			//end drag
+			drag.targetCell = drag.targetCell || drag.sourceCell;
+			this.swapCells(drag.sourceCell, drag.targetCell);
+			drag.sourceCell.classList.remove('dragSource');
+			drag.sourceCell = null;
+			drag.targetCell = null;
+		} else {
+			//continue drag
+			drag.targetCell = dragCell;
+		}
+	}
+
+	swapCells(a, b) {
+		const rA = +a.getAttribute('r');
+		const cA = +a.getAttribute('c');
+		const rB = +b.getAttribute('r');
+		const cB = +b.getAttribute('c');
+		const { board } = this.state;
+		this.setState({board: 
+			board.map((row, r) => row.map((cell, c) => {
+				if (r === rA && c === cA) return board[rB][cB];
+				if (r === rB && c === cB) return board[rA][cA];
+				return cell;
+			}))
+		});
 	}
 
 	componentDidMount() {
@@ -31,7 +70,7 @@ export default class Main extends Component {
 	render() {
 		const { board } = this.state;
 		return (
-			<Board board={board} />
+			<Board board={board} onDragCellChanged={this.onDragCellChanged.bind(this)} />
 		);
 	}
 }
